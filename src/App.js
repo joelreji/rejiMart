812fbba7d3bcdbaac1cd134d393297b5ff2f4d6f
joelreji <joelreji97@gmail.com>
2023-07-18 22:31:35 -0400

    AlgoMart

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
             <Col className="col-md-12 bg-white">
               <div className="container pt-3">
                 <div className="text-container p-3">
-                  <h1 className="display-2 fw-bold pb-1 custom-color m-2">Welcome to RejiMart</h1>
+                  <h1 className="display-2 fw-bold pb-1 custom-color m-2">Welcome to AlgoMart</h1>
                   <p className="pb-1 pt-4 m-2" style={{ fontSize: '25px' }}>
                     This marketplace is built to showcase the true power of <a href="http://www.heimdallapp.org">Heimdall</a></p>
                 </div>
@@ -27,9 +27,9 @@ function App() {
             <Card.Body>
               <Card.Title className="custom-color fw-bold display-3 m-3 pb-4 row-2">Mission</Card.Title>
               <Card.Text className="pb-2" style={{ fontSize: '25px' }}>
-                RejiMart is the idea that birthed Heimdall. Heimdall was built to serve the greater purpose of being the backbone of RejiMart.
-                The goal of RejiMart is to create a platform for the sale of incredible algorithms. Heimdall's machine learning models will
-                be the first to be integrated into RejiMart. All algorithms on RejiMart will be onboarded onto the <strong>Polygon Blockchain</strong> and the owner
+                AlgoMart is the idea that birthed Heimdall. Heimdall was built to serve the greater purpose of being the backbone of AlgoMart.
+                The goal of AlgoMart is to create a platform for the sale of incredible algorithms. Heimdall's machine learning models will
+                be the first to be integrated into AlgoMart. All algorithms on AlgoMart will be onboarded onto the <strong>Polygon Blockchain</strong> and the owner
                 will be able to utilize the algorithm for their specific needs. 
               </Card.Text>
             </Card.Body>
