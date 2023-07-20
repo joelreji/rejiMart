3237032b3444971f0f1c6e4177295157226b982b
joelreji <joelreji97@gmail.com>
2023-07-20 13:12:26 -0400

    Adding Sleep Disorder Widget

---
+++ src/components/Demo.jsx
@@ -0,0 +1,40 @@
+import './Components.css';
+import { Container, Row, Col, Accordion } from "react-bootstrap"
+import SleepPred from './SleepPred';
+
+const Demo = () => {
+
+  return (
+    <div className="App">
+      <div className="pb-5">
+        <Container>
+          <Row className="flex-column-reverse flex-md-row justify-content-center">
+            <Col className="col-md-12 bg-white">
+              <div className="container pt-3">
+                <div className="text-container p-3">
+                  <h1 className="display-2 fw-bold pb-1 custom-color m-2">Heimdall Plugins</h1>
+                  <p className="pb-1 pt-4 m-2 pb-5" style={{ fontSize: '25px' }}>
+                    This plugin store is built to showcase the true power of <a href="http://www.heimdallapp.org">Heimdall</a></p>
+                </div>
+              </div>
+            </Col>
+          </Row>
+          <Row xs={1} md={2} className="g-4">
+            <Col>
+              <Accordion defaultActiveKey="0" className="pb-5">
+                <Accordion.Item eventKey="0">
+                  <Accordion.Header>Are you at risk for a sleep disorder?</Accordion.Header>
+                  <Accordion.Body>              
+                    <SleepPred></SleepPred>
+                  </Accordion.Body>
+                </Accordion.Item>
+              </Accordion>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </div>
+  );
+}
+
+export default Demo;
