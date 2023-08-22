9ed461c6a79556b4751c03c233a4842d45b0c566
joelreji <65984611+joelreji@users.noreply.github.com>
2023-08-22 12:40:59 -0400

    Removing Home

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import Home from "./components/Home";
 import Demo from "./components/Demo";
 import Trophy from "./components/Trophy";
 
@@ -11,7 +10,6 @@ import Trophy from "./components/Trophy";
 function App() {
   return (
     <Switch>
-    <Route exact path="/" component={Home} />
     <Route exact path="/plugins" component={Demo} />
     <Route exact path="/trophy" component={Trophy} />
     </Switch>
